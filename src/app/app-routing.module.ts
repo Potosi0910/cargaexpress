4ada434046c1364c54317c952ea6eb13f23f605e
Potosi0910 <didier0910@hotmail.com>
2024-01-30 16:05:14 -0500

    3.0 version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,41 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './modulos/principal.component';
 import { DashboardComponent } from './modulos/dashboard/dashboard.component';
-import { LoginComponent } from './modulos/login/login.component';
+import { CerrarSesionComponent } from './modulos/cerrar-sesion/cerrar-sesion.component';
+import { ReporteComponent } from './modulos/contabilidad/reporte/reporte.component';
+import { SoporteComponent } from './modulos/contabilidad/soporte/soporte.component';
+import { BandejaRrhhComponent } from './modulos/rrhh/bandeja-rrhh/bandeja-rrhh.component';
+import { IngresoComponent } from './modulos/rrhh/ingreso/ingreso.component';
+import { LiquidacionComponent } from './modulos/rrhh/liquidacion/liquidacion.component';
+import { BandejaComponent } from './modulos/soporte-tecnico/bandeja/bandeja.component';
+import { NuevoTicketComponent } from './modulos/soporte-tecnico/nuevo-ticket/nuevo-ticket.component';
+import { PermisosSistemaComponent } from './modulos/soporte-tecnico/permisos-sistema/permisos-sistema.component';
+import { UsuariosComponent } from './modulos/usuarios/usuarios.component';
 
 const routes: Routes = [
-{
+  {
     path: '', component: PrincipalComponent,
-      children: [
-        { path: 'dashboard', component: DashboardComponent },
-        {path: '', redirectTo: 'dashboard', pathMatch: 'full' }
-       
-      ] 
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'contabilidad/reporte', component: ReporteComponent }, 
+      { path: 'contabilidad/soporte', component: SoporteComponent},
+      
+      { path: 'rrhh/bandeja-rrhh', component: BandejaRrhhComponent},
+      { path: 'rrhh/ingreso', component: IngresoComponent},
+      { path: 'rrhh/liquidacion', component: LiquidacionComponent},
 
-}
-
+      { path: 'soporte-tecnico/bandeja', component: BandejaComponent},
+      { path: 'soporte-tecnico/nuevo-ticket', component: NuevoTicketComponent},
+      { path: 'soporte-tecnico/permisos-sistema', component: PermisosSistemaComponent},
+      { path: 'usuarios', component: UsuariosComponent},
+      { path: 'cerrar-sesion', component: CerrarSesionComponent }
+    ]
+  },
+  {
+    path: 'Reporte', component: ReporteComponent
+  },
 ];
-
-
-
-//const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,16 @@ import { FooterComponent } from './estructura/footer/footer.component';
 import { PrincipalComponent } from './modulos/principal.component';
 import { DashboardComponent } from './modulos/dashboard/dashboard.component';
 import { LoginComponent } from './modulos/login/login.component';
+import { CerrarSesionComponent } from './modulos/cerrar-sesion/cerrar-sesion.component';
+import { NuevoTicketComponent } from './modulos/soporte-tecnico/nuevo-ticket/nuevo-ticket.component';
+import { BandejaComponent } from './modulos/soporte-tecnico/bandeja/bandeja.component';
+import { PermisosSistemaComponent } from './modulos/soporte-tecnico/permisos-sistema/permisos-sistema.component';
+import { UsuariosComponent } from './modulos/usuarios/usuarios.component';
+import { IngresoComponent } from './modulos/rrhh/ingreso/ingreso.component';
+import { LiquidacionComponent } from './modulos/rrhh/liquidacion/liquidacion.component';
+import { BandejaRrhhComponent } from './modulos/rrhh/bandeja-rrhh/bandeja-rrhh.component';
+import { ReporteComponent } from './modulos/contabilidad/reporte/reporte.component';
+import { SoporteComponent } from './modulos/contabilidad/soporte/soporte.component';
 
 
 @NgModule({
@@ -21,7 +31,17 @@ import { LoginComponent } from './modulos/login/login.component';
     FooterComponent,
     PrincipalComponent,
     DashboardComponent,
-    LoginComponent
+    LoginComponent,
+    CerrarSesionComponent,
+    NuevoTicketComponent,
+    BandejaComponent,
+    PermisosSistemaComponent,
+    UsuariosComponent,
+    IngresoComponent,
+    LiquidacionComponent,
+    BandejaRrhhComponent,
+    ReporteComponent,
+    SoporteComponent
   ],
   imports: [
     BrowserModule,
